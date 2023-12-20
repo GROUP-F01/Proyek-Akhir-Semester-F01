99d96229dbe3300b078d0c64e20ffd1b32252c5c
m-hilmy-erryanto <muhammad.hilmy22@ui.ac.id>
2023-12-20 18:21:56 +0700

    fix card icha

--- lib/wishlist/widgets/wishlist_card.dart
+++ lib/wishlist/widgets/wishlist_card.dart
@@ -15,53 +15,54 @@ class WishlistCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.green,
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(5, 5),
-          ),
-        ],
-      ),
+    print(bookImageUrl);
+    return Card(
+      elevation: 3,
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+        side: BorderSide(
+          color: Colors.grey,
+          width: 2.0,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
             bookImageUrl,
-            width: 200,
-            height: 300, 
-          ),
-          const SizedBox(height: 10),
-          Text(
-            "Category: $bookCategory",
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
+            width: 175,
+            height: 300,
             ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            "Title: $bookTitle",
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
+            const SizedBox(height: 10),
+            Text(
+              "Category: $bookCategory",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            "Reason: $reason",
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
+            const SizedBox(height: 10),
+            Text(
+              "Title: $bookTitle",
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Text(
+              "Reason: $reason",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
